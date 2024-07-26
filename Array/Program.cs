c82883f36c78bebe695bf3da946b8af07505f0da
arthu <arthu@ARTHUR>
2024-07-25 21:29:13 -0300

    Adicionar arquivos de projeto.

---
+++ Array/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string produto1 = "Sea of thieves ";
+            string produto2 = "fifa";
+            string produto3 = "minecraft";
+            string produto4 = "half-life";
+            string produto5 = "cs";
+
+            string[] produtos = new string[] {
+                produto1,
+                produto2,
+                produto3, 
+                produto4,
+                produto5 
+            };
+
+        }
+    }
+}
